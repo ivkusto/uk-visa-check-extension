f9564ef313f407fd17513535875b74c0c947422a
ivkusto <zhakovnikita@gmail.com>
2023-04-25 12:53:18 +0000

    Build: (cd50886) remove unnecessary import

--- assets/content.tsx-loader-d26cc092.js
+++ assets/content.tsx-loader-d26cc092.js
@@ -5,7 +5,7 @@
   (async () => {
     const { onExecute } = await import(
       /* @vite-ignore */
-      chrome.runtime.getURL("assets/content.tsx-24113c95.js")
+      chrome.runtime.getURL("assets/content.tsx-2391ba7c.js")
     );
     onExecute?.({ perf: { injectTime, loadTime: performance.now() - injectTime } });
   })().catch(console.error);
